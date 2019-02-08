5279b70daac27112dfc5023f4d2513e1806f6d2d
re-z <re-z@ukr.net>
2019-02-08 23:32:23 +0200

    full ajax

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 button.addEventListener('click', function(){
-    
-let xhr = new XMLHttpRequest();
-xhr.onload(function(){
-    
-})
-    
-
+    //init
+    let xhr = new XMLHttpRequest();
+    //open
+    xhr.open('GET', 'data.txt', true)
+    // statement
+    xhr.onload = function() {  
+        if (this.status === 200) {
+            alert(1)
+            output.innerHTML = this.responseText;
+        } else {
+            alert( 1111 )
+        }
+    }
+    //send
+    xhr.send();
 })
