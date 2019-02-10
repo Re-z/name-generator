fa25f3764ba374c788066ae35a79b56cb2fcd90a
re-z <re-z@ukr.net>
2019-02-10 20:33:10 +0200

    using ajax\json with foreach and looping

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let btn = document.getElementById('button1');
+let btn2 = document.getElementById('button2');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', printSingleJSON)
+btn2.addEventListener('click', printMultipleJSON)
 
+function printSingleJSON() {
+    //init
     let xhr = new XMLHttpRequest();
-
+    //open
     xhr.open('GET', 'employee.json', true);
-    
+    //statement
     xhr.onload = function(){
         if(this.status === 200) {
             let response = JSON.parse(this.responseText),
@@ -21,5 +25,35 @@ btn.addEventListener('click', function(){
             `
         }
     }
+    //send
     xhr.send()
-})
+}
+
+function printMultipleJSON(){
+    let xhr = new XMLHttpRequest();
+
+    //open
+    xhr.open('GET', 'employees.json', true);
+    //statement
+    xhr.onload = function(){
+        if(this.status === 200) {
+            let arr = JSON.parse(this.responseText);
+            let outputString = '';
+            
+            arr.forEach(function(el){
+                outputString += `
+                <ul>
+                    <li> id: ${el.id} </li>
+                    <li> name: ${el.name} </li>
+                    <li> company: ${el.company} </li>
+                    <li> job: ${el.job} </li>
+                </ul>
+                </hr>    
+                `                
+            })
+            employees.innerHTML = outputString;
+        }
+    }
+    
+    xhr.send();
+}
