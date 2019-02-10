bfcdc1bd2842bf43d417e870ccfd273eee1086cb
re-z <re-z@ukr.net>
2019-02-10 19:29:50 +0200

    simple ajax\json example

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-button.addEventListener('click', function(){
-    //init
+let btn = document.getElementById('button1');
+
+btn.addEventListener('click', function(){
+
     let xhr = new XMLHttpRequest();
-    //open
-    xhr.open('GET', 'data.txt', true)
-    // statement
-    xhr.onload = function() {  
-        if (this.status === 200) {
-            alert(1)
-            output.innerHTML = this.responseText;
-        } else {
-            alert( 1111 )
+
+    xhr.open('GET', 'employee.json', true);
+    
+    xhr.onload = function(){
+        if(this.status === 200) {
+            let response = JSON.parse(this.responseText),
+                output = document.getElementById('employee');
+            
+            output.innerHTML = `
+            <ul>
+                <li>id: ${response.id} </li>
+                <li>name: ${response.name} </li>
+                <li>company: ${response.company} </li>
+                <li>job: ${response.job} </li>
+            </ul>
+            `
         }
     }
-    //send
-    xhr.send();
+    xhr.send()
 })
